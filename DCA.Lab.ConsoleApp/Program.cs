dcfa442f1c623d1015e28b78afaa596019a4342e
Elvis <elvis.gabriel@doctorcareanywhere.com>
2018-03-22 14:38:51 +0000

    2nd change to the app

--- DCA.Lab.ConsoleApp/Program.cs
+++ DCA.Lab.ConsoleApp/Program.cs
@@ -10,7 +10,8 @@ namespace DCA.Lab.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.Write("First change to the app!");
+            Console.Write("1st change to the app!");
+            Console.Write("2nd change to the app!");
             Console.ReadKey();
         }
     }
