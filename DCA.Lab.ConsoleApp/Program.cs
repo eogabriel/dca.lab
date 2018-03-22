f7b1d0cfee44f23a9e8449cc0943e29e80193776
Elvis Gabriel <37081930+eogabriel@users.noreply.github.com>
2018-03-22 16:23:49 +0000

    This is just a test

--- DCA.Lab.ConsoleApp/Program.cs
+++ DCA.Lab.ConsoleApp/Program.cs
@@ -17,7 +17,7 @@ namespace DCA.Lab.ConsoleApp
             Console.Write("5th change to the app!");
             Console.Write("6th change to the app!");
             Console.Write("7th change to the app!");
-            Console.Write("8th change to the app!");
+            Console.Write("8th change to the app!@");
             Console.ReadKey();
         }
     }
